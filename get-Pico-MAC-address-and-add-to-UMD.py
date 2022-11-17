5ade43d0bbb8d89bbedbabfdd764035d2b754240
ColeFeely6 <65377055+ColeFeely6@users.noreply.github.com>
2022-11-17 17:47:35 -0500

    Create get-Pico-MAC-address-and-add-to-UMD.py

---
+++ get-Pico-MAC-address-and-add-to-UMD.py
@@ -0,0 +1,8 @@
+from getmac import get_mac_address as gma
+print(gma())
+
+# Make sure that you are in the Python 3 interepreter by going into Preferences > Interpreter
+# Make sure to switch back to RPI Pico after for your other code
+# Go to add: https://www.umass.edu/it/news/20180119/connectgamingconsolesstreamingdeviceswirelessnetworkresidentialareas
+# Add UMass device as other and give the Mac address
+
