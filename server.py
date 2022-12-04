af3c2a82b7fae9e8690402255094a2fb70cfd66d
PeaceIdahor <70335777+PeaceIdahor@users.noreply.github.com>
2022-12-04 16:38:55 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-# Download python-pystone_lowmem package
-# https://thepihut.com/blogs/raspberry-pi-tutorials/wireless-communication-between-two-raspberry-pi-pico-w-boards
-# Mac can only do server for now 
-
+# Webserver to send RGB data
+# Tony Goodhew 5 July 2022
 import network
 import socket
 import time
 from machine import Pin, ADC
-#from secret import ssid,password
 import random
+import gc
+import urequests as requests
+import json
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect('', '')
+wlan.connect("PeaceisAwesome", "32439344")
        
 # Wait for connect or fail
 max_wait = 10
@@ -39,25 +39,35 @@ s.listen(1)
 print('listening on', addr)
 
 # Listen for connections
+def notification():
+    gc.collect()
+    resp = None
+    try:
+        body = "Sensor Data"
+        title = "Fall Detected"
+        data_sent = {"type":"note","title":title,"body":body}
+        API_KEY = 'o.Ywi05DOBMqM3jdGJ5FdGEwd6STLclHuj'
+        pb_headers = {
+        'Authorization': 'Bearer ' + API_KEY,
+        'Content-Type': 'application/json',
+        'Host': 'api.pushbullet.com'
+        }
+        resp = requests.post('https://api.pushbullet.com/v2/pushes',data=json.dumps(data_sent),headers=pb_headers)
+    except Exception as e:
+        if isinstance(e, OSError) and resp:
+            resp.close()
+        value = {"error":e}
+    gc.collect()
+    return
 while True:
     try:
         cl, addr = s.accept()
         print('client connected from', addr)
         request = cl.recv(1024)
         print(request)
-        # Do not unpack request
-        # We reply to any request the same way
-        # Generate 3 values to send back
-        r = random.randint(0,255)
-        g = random.randint(0,255)
-        b = random.randint(0,255)
-        # Join to make a simple string with commas as separators
-        rgb = str(r) + "," + str(g) + ","+str(b)
-        
-        response = rgb # This is what we send in reply
-
+        notification()
+        response = "Alerted Caregiver"
         cl.send(response)
-        print("Sent:" + rgb)
         cl.close()
 
     except OSError as e:
