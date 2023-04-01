de09463fe5d55a04b62e791460223a14594905fa
PeaceIdahor <70335777+PeaceIdahor@users.noreply.github.com>
2023-04-01 15:31:11 -0400

    Update mmwave.ino

--- CDR/mmwave.ino
+++ CDR/mmwave.ino
@@ -3,51 +3,57 @@
 
 const char* ssid = "PeaceisAwesome";
 const char* password = "32439344";
-const char* server_address = "192.168.137.79";
+const char* server_address = "192.168.137.76";
 int server_port = 80;
 FallDetectionRadar radar;
-
-void setup()
-{
+WiFiClient client;
+void setup() {
   radar.SerialInit();
   Serial.begin(9600);
   delay(1500);
-  Serial.println("Readly");
-  WiFi.begin(ssid, password);
+  Serial.println("Ready");
 
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to Wifi...");
+  bool wifiConnected = false;
+
+  while (!wifiConnected) {
+    WiFi.begin(ssid, password);
+
+    Serial.println("Connecting to WiFi...");
+    for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println();
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Connected to WiFi!");
+      wifiConnected = true;
+    }
+    else {
+      Serial.println("Failed to connect to WiFi. Retrying in 10 seconds...");
+      delay(10000);
+    }
   }
-
-  Serial.println("Connected to WiFi!");
 }
 
-void loop()
-{
-  WiFiClient client;
+void loop() {
+  radar.recvRadarBytes(); //Receive radar data and start processing
+  if (radar.newData == true) { //The data is received and transferred to the new list dataMsg[]
+    byte dataMsg[radar.dataLen + 1] = { 0x00 };
+    dataMsg[0] = 0x55; //Add the header frame as the first element of the array
+    for (byte n = 0; n < radar.dataLen; n++)
+      dataMsg[n + 1] = radar.Msg[n]; //Frame-by-frame transfer
+    radar.newData = false; //A complete set of data frames is saved
 
- //Serial.println("Connected to server");
- radar.recvRadarBytes();                       //Receive radar data and start processing
- if (radar.newData == true) {                  //The data is received and transferred to the new list dataMsg[]
-    byte dataMsg[radar.dataLen+1] = {0x00};
-    dataMsg[0] = 0x55;                         //Add the header frame as the first element of the array
-    for (byte n = 0; n < radar.dataLen; n++)dataMsg[n+1] = radar.Msg[n];  //Frame-by-frame transfer
-    radar.newData = false;                     //A complete set of data frames is saved
-    
-    radar.ShowData(dataMsg);                 //Serial port prints a set of received data frames
-    if (Serial.available()){
-      String message = Serial.readStringUntil('\n');
-      if (message.indexOf("A suspected fall was detected.")==-1){
-          if (!client.connect(server_address, server_port)){
-              Serial.println("Failed to connect to server");
-              delay(250);
-              return;
-              }
-              Serial.println("Connected to server");
-        }
+    radar.ShowData(dataMsg); //Serial port prints a set of received data frames
+    int returned = radar.Fall_inf(dataMsg); //Sleep information output
+    if (returned == 1){
+       while (!client.connected()){
+          Serial.println("Connecting to server");
+          client.connect(server_address, server_port);
+          delay(1000); // Wait 1 second before trying again
+       }
+       Serial.println("Connected to server");
       }
-    radar.Fall_inf(dataMsg);                  //Sleep information output
   }
-  client.stop();
 }
